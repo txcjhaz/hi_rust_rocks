b39101e674e3195ded0489254078424ce6038511
Junhui Chen <384673977@qq.com>
2022-07-04 00:22:26 +0800

    假装解决

--- examples/hi_rust_rocks.rs
+++ examples/hi_rust_rocks.rs
@@ -4,6 +4,8 @@ use may_minihttp::{HttpService, HttpServiceFactory, Request, Response, KvUtil, M
 use rocksdb::DB;
 use serde::{Deserialize, Serialize};
 
+use lazy_static::lazy_static;
+
 extern crate serde;
 
 // #[derive(Serialize)]
@@ -11,9 +13,7 @@ extern crate serde;
 //     message: &'static str,
 // }
 
-struct Techempower<'a> {
-    kv: &'a RocksdbUtil
-}
+struct Techempower {}
 
 #[derive(Deserialize, Serialize, Debug)]
 struct KeyValue<'a> {
@@ -35,7 +35,7 @@ struct ZRangeScore {
 
 
 
-impl HttpService for Techempower<'_> {
+impl HttpService for Techempower {
 
     fn call(&mut self, req: Request, rsp: &mut Response) -> io::Result<()> {
         // Bare-bones router
@@ -44,7 +44,7 @@ impl HttpService for Techempower<'_> {
         }
         else if req.path().starts_with("/query/") {
             let key = &req.path()[7..];
-            let val = self.kv.get(key);
+            let val = ROCKS.get(key);
             let b = rsp.body_mut();
             b.write_str(&val).unwrap(); // TODO err handle
             // println!("key is {}, val is {}", key, val);
@@ -54,19 +54,19 @@ impl HttpService for Techempower<'_> {
             let r_body = req.body_();
             // println!("body is {}", std::str::from_utf8(&r_body.to_vec()).unwrap());
             let kv: KeyValue = serde_json::from_slice(r_body).unwrap();  // FIXME 处理异常         
-            self.kv.set(kv.key, kv.value);
+            ROCKS.set(kv.key, kv.value);
             // println!("to add key is {}, value is {}", kv.key, kv.value);
         }
         else if req.path().starts_with("/del/") {
             let key = &req.path()[7..];
-            self.kv.remove(key);
+            ROCKS.remove(key);
             // println!("del key is {}", key);
         }
         else if req.path() == "/list" {
             let r_body = req.body_();
             let keys: Vec<&str> = serde_json::from_slice(r_body).unwrap();  // FIXME 处理异常         
             
-            let vals = self.kv.mget(&keys);
+            let vals = ROCKS.mget(&keys);
 
             let mut resp = Vec::<KeyValue>::new();
             let mut i = 0;
@@ -96,7 +96,7 @@ impl HttpService for Techempower<'_> {
                 vals.push(p.value);
             }
 
-            self.kv.mset(&keys, &vals);
+            ROCKS.mset(&keys, &vals);
         }
         else if req.path().starts_with("/zadd/") {
             let key = &req.path()[6..];
@@ -104,7 +104,7 @@ impl HttpService for Techempower<'_> {
             // println!("key is {}, body is {}", key, std::str::from_utf8(&r_body.to_vec()).unwrap());
             let z_val: ZValue = serde_json::from_slice(r_body).unwrap();  // FIXME 处理异常
 
-            self.kv.zadd(key, z_val.value, &z_val.score);
+            ROCKS.zadd(key, z_val.value, &z_val.score);
         }
         else if req.path().starts_with("/zrange/") {
             let key = &req.path()[8..];
@@ -112,13 +112,13 @@ impl HttpService for Techempower<'_> {
             println!("key is {}, body is {}", key, std::str::from_utf8(&r_body.to_vec()).unwrap());
             let z_score: ZRangeScore = serde_json::from_slice(r_body).unwrap();  // FIXME 处理异常
 
-            self.kv.zrange(key, &z_score.min_score, &&z_score.max_score);
+            ROCKS.zrange(key, &z_score.min_score, &&z_score.max_score);
         }
         else if req.path().starts_with("/zrmv/") {
             let keyAndValue = &req.path()[6..];
             let splits: Vec<&str> = keyAndValue.split('/').collect();
             // println!("key is {}, val is {}", splits[0], splits[1]);
-            self.kv.zrmv(splits[0], splits[1]);
+            ROCKS.zrmv(splits[0], splits[1]);
         }
         else {
             rsp.status_code("404", "Not Found");
@@ -128,31 +128,34 @@ impl HttpService for Techempower<'_> {
     }
 }
 
-struct HttpServer<'a> {
-    db: &'a RocksdbUtil
+struct HttpServer {
 }
 
-impl HttpServiceFactory for HttpServer<'_> {
-    type Service<'a> = Techempower<'a>;
+impl HttpServiceFactory for HttpServer {
+    type Service = Techempower;
 
     fn new_service(&self) -> Self::Service {
         // let kv_util_impl = MockKvUtil {};
-        Techempower { kv: self.db }
+        Techempower {}
     }
 
 }
 
 fn main() {
-    let mut rocksdb = 
-    DB::open_default("C:/Users/txcjh/Desktop/Projects/may_minihttp/storage").unwrap();
-    println!("a lei ? {}", String::from_utf8(rocksdb.get("test").unwrap().unwrap()).unwrap());
-
+    ROCKS.set("test", "666");
     may::config()
         .set_pool_capacity(10000)
         .set_stack_size(0x1000);
-    let http_server = HttpServer { db: &RocksdbUtil { db: 
-        rocksdb
-    }};
+    let http_server = HttpServer {};
     let server = http_server.start("0.0.0.0:8081").unwrap();
     server.join().unwrap();
+}
+
+lazy_static!{
+    static ref ROCKS: RocksdbUtil = {
+        println!("rocksdb init");
+        let db = DB::open_default("C:/Users/txcjh/Desktop/Projects/may_minihttp/storage").unwrap();
+        println!("rocksdb init successfully");
+        RocksdbUtil { db: db }
+    };
 }
