1b1bd9bba4f30db0ab0c7e03f313f6ba08926c08
Junhui Chen <384673977@qq.com>
2022-07-07 23:50:38 +0800

    增加优化参数

--- examples/hi_rust_rocks.rs
+++ examples/hi_rust_rocks.rs
@@ -118,6 +118,7 @@ impl HttpService for Techempower {
             match json_parse_resp {
                 Ok(keys) => {
                     let vals = ROCKS.multi_get(keys.clone());
+                    let mut resp = Vec::<String>::new();
                     
                     if vals.len() == 0 {
                         let f = b.write_str("[]");
@@ -131,16 +132,6 @@ impl HttpService for Techempower {
                         return Ok(());
                     }
 
-                    let f = b.write_char('[');
-                    match f {
-                        Ok(()) => {},
-                        Err(_err) => {
-                            rsp.status_code("404", "");
-                            return Ok(());
-                        }
-                    }
-                    
-                    let len = keys.len() - 1;
                     for (i, val_resp) in vals.iter().enumerate() {
                         match val_resp {
                             Ok(maybe_val) => {
@@ -149,27 +140,7 @@ impl HttpService for Techempower {
                                         let val_str = std::str::from_utf8(val).unwrap();
                                         let kv_str = format!("{{\"key\":\"{}\",\"value\":\"{}\"}}", keys[i], val_str);
                                         
-                                        let f = b.write_str(&kv_str);
-
-                                        match f {
-                                            Ok(()) => {},
-                                            Err(_err) => {
-                                                rsp.status_code("404", "");
-                                                return Ok(());
-                                            }
-                                        }
-
-                                        if i < len  {
-                                            let f = b.write_char(',');
-                                            match f {
-                                                Ok(()) => {},
-                                                Err(_err) => {
-                                                    rsp.status_code("404", "");
-                                                    return Ok(());
-                                                }
-                                            }
-                                        }
-
+                                        resp.push(kv_str);
                                     },
                                     None => {
                                         rsp.status_code("404", "");
@@ -184,9 +155,13 @@ impl HttpService for Techempower {
                         }
                     }
 
-                    let f = b.write_char(']');
-                    match f {
-                        Ok(()) => {},
+                    let json_str_resp = serde_json::to_string(&resp);
+                    match json_str_resp {
+                        Ok(s) => {
+                            b.write_str(&s).unwrap();
+                            rsp.header("Content-Type: application/json");
+                            return Ok(());
+                        },
                         Err(_err) => {
                             rsp.status_code("404", "");
                             return Ok(());
@@ -198,8 +173,7 @@ impl HttpService for Techempower {
                     return Ok(());
                 }
             }
-
-            rsp.header("Content-Type: application/json");
+            return Ok(());
         }
         else if req.path() == "/batch" {
             let r_body = req.body_();
@@ -257,7 +231,7 @@ fn main() {
     may::config()
         .set_pool_capacity(10000)
         .set_stack_size(0x1000)
-        .set_workers(3);
+        .set_workers(1);
     let http_server = HttpServer {};
     let server = http_server.start("0.0.0.0:8080").unwrap();
     server.join().unwrap();
@@ -266,6 +240,36 @@ fn main() {
 lazy_static!{
     static ref ROCKS: rocksdb::DB = {
         println!("rocksdb init");
+
+        let mut db_opts = rocksdb::Options::default();
+
+        // use direct io (cannot use with mmap at the same time)
+        db_opts.set_use_direct_reads(true);
+        db_opts.set_use_direct_io_for_flush_and_compaction(true);
+        db_opts.set_compaction_readahead_size(2 << 20);
+        db_opts.set_writable_file_max_buffer_size(1 << 20);
+
+        // block cache setting
+        let mut block_cache_opts = rocksdb::BlockBasedOptions::default();
+        let lru_cache = rocksdb::Cache::new_lru_cache(5 << 30).unwrap();
+        block_cache_opts.set_block_cache(&lru_cache);
+        block_cache_opts.set_bloom_filter(10.0, false);
+        block_cache_opts.set_block_size(16 << 10);
+        block_cache_opts.set_cache_index_and_filter_blocks(true);
+        block_cache_opts.set_pin_l0_filter_and_index_blocks_in_cache(true);
+        block_cache_opts.set_format_version(5);
+        db_opts.set_block_based_table_factory(&block_cache_opts);
+
+        // rate limiter setting, usually change first param
+        db_opts.set_ratelimiter(1 << 20, 100 * 1000, 10);
+
+        // general opt
+        db_opts.set_max_background_jobs(2);
+        db_opts.set_bytes_per_sync(1 << 20);
+
+        // wal setting
+        db_opts.set_wal_bytes_per_sync(1 << 20);
+
         let db = DB::open_default("/data").unwrap();
         println!("rocksdb init successfully");
         db
