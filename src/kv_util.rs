094c645ce0b7cad75db4088b3a2363b432cf2515
Junhui Chen <384673977@qq.com>
2022-07-09 21:03:24 +0800

    a

--- src/kv_util.rs
+++ src/kv_util.rs
@@ -1,3 +1,7 @@
+use std::{hash::Hash, collections::{hash_map::DefaultHasher, HashMap}};
+
+use rocksdb::WriteBatch;
+
 pub trait KvUtil {
     fn set(&self, key: &str, value: &str);
     fn get(&self, key: &str) -> &str;
@@ -46,41 +50,122 @@ impl KvUtil for MockKvUtil {
     }
 }
 
-pub struct RocksdbUtil {
-    pub db: rocksdb::DB
+pub struct RocksdbUtil<'a> {
+    pub dbs: Vec<&'a rocksdb::DB>,
+    pub size: u32
 }
 
-impl RocksdbUtil {
+impl RocksdbUtil<'_> {
+
+    fn getDb(&self, key: &str) -> &rocksdb::DB {
+        let index = self.BKDRHash(key) % self.size;
+        let db_opt = self.dbs.get(index as usize);
+        
+        match db_opt {
+            Some(db) => {
+                return db;
+            }
+            None => {
+                return self.dbs[0];
+            }
+        }
+
+    }
+
+    fn BKDRHash(&self, str: &str) -> u32 {
+        let seed: u32 = 131;
+        let mut hash: u32 = 0;
+
+        for ch in str.as_bytes() {
+            hash = hash * seed + (*ch as u32);
+        }
+
+        hash & 0x7FFFFFFF
+    }
+ 
     pub fn set(&self, _key: &str, _value: &str) {
-        self.db.put(_key, _value).unwrap();
+        self.getDb(_key).put(_key, _value).unwrap();
     }
 
     pub fn get(&self, _key: &str) -> String {
-       let s = self.db.get(_key).unwrap().unwrap();
+       let s = self.getDb(_key).get(_key).unwrap().unwrap();
        let q = String::from_utf8(s).unwrap();
        return q;
     }
 
     pub fn remove(&self, _key: &str) {
-        self.db.delete(_key).unwrap();
+        self.getDb(_key).delete(_key).unwrap();
     }
 
-    pub fn mget(&self, _keys: &Vec<&str>) -> Vec<String> {
-        let mut mock_res = Vec::new();
-        let resp = self.db.multi_get(_keys);
-        for v in resp {
-            let val = v.unwrap().unwrap();
-            let l = String::from_utf8(val).unwrap();
-            mock_res.push(l);
+    pub fn mget(&self, _keys: &Vec<&str>) -> std::result::Result<&Vec<(usize, &Vec<u8>)>, &'static str> {
+        let mut key_map: HashMap<u32, Vec<&str>> = HashMap::with_capacity(self.size as usize);
+        for k in (*_keys) {
+            let slot = self.BKDRHash(k);
+            let opt = key_map.get_key_value(&slot);
+            match opt {
+                Some(v) => {
+                    let mut vec = v.1;
+                    vec.push(k);
+                },
+                None => {
+                    key_map.insert(slot, vec![k]);
+                }
+            }
         }
-        mock_res
+
+        let mut mock_res = Vec::new();
+        for _key in key_map {
+            let (i, ks) = _key;
+            let db = self.dbs[i as usize];
+            let res = db.multi_get(ks);
+
+            for (i, item) in res.iter().enumerate() {
+                match item {
+                    Ok(val) => {
+                        match val {
+                            Some(v) => {
+                                mock_res.push((i, v));
+                            },
+                            None => {
+                                return Err("404");
+                            }
+                        }
+                    },
+                    Err(_err) => {
+                        return Err("404");
+                    }
+                }
+            }
+        }
+
+        return Ok(&(mock_res.clone()));
     }
 
-    pub fn mset(&self, _keys: &Vec<&str>, _vals: &Vec<&str>) {
-        let mut i = 0;
-        while i < _keys.len() {
-            self.db.put(_keys[i], _vals[i]).unwrap();
-            i = i + 1;
+    pub fn mset(&self, kvs: &Vec<(&str, &str)>) {
+        let key_map: HashMap<u32, Vec<(&str, &str)>> = 
+            HashMap::with_capacity(self.size as usize);
+
+        for kv in kvs {
+            let slot = self.BKDRHash(kv.0);
+            let opt = key_map.get_key_value(&slot);
+            match opt {
+                Some(v) => {
+                    let mut vec = v.1;
+                    vec.push(*kv);
+                },
+                None => {
+                    key_map.insert(slot, vec![*kv]);
+                }
+            }
+        }
+
+        for _key in key_map {
+            let db = self.dbs[_key.0 as usize];
+            let mut batch = WriteBatch::default();
+            for t in _key.1 {
+                batch.put(t.0, t.1);
+            }
+            db.write(batch);
         }
     }
 
